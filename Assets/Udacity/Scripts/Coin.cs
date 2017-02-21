045d23f2bf886f3809b405fe6d0f99e43349f7d5
soheilsalehinia2012 <soheilsalehinia2012@gmail.com>
2017-02-22 02:45:54 +0330

    Edited coin.cs - created CoinPoof Prefab

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Coin : MonoBehaviour 
 {
     //Create a reference to the CoinPoofPrefab
+	public GameObject coinPoof;
+	public GameObject coin;
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
         // Make sure the poof animates vertically
         // Destroy this coin. Check the Unity documentation on how to use Destroy
+		Object.Instantiate (coinPoof, coin.transform.position , Quaternion.Euler(-90, 0, 0 ));
+		Destroy (coin);
+
     }
 
 }
